f073c713be45d1659d09286c2e9cb5f87828ab76
Oleksandr Budonnyi <ob4@cs1-17-silver14.cs.ucc.ie>
2025-04-14 15:57:23 +0100

    Add small comments in main block

--- backend/extractor/merged/extract_snapshots.py
+++ backend/extractor/merged/extract_snapshots.py
@@ -83,8 +83,10 @@ class PrinterSnapshotter:
 
 
 if __name__ == "__main__":
+    #Constructor
     url = "http://143.239.73.224:8080/?action=snapshot"
     output_file = "printer_images.h5"
 
+    #Initialize functions invocation
     snapper = PrinterSnapshotter(url, output_file)
     snapper.start_capturing()
