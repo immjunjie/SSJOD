76f8f8c58ddd1b59c2a27556b8a847b42292de34
immjunjie <122109215@umail.ucc.ie>
2025-04-15 12:35:44 +0100

    Refactor(pullFromPrint.py): Modify relevant file path
    - Add the universal method to get access the files with relative path for gcode_path and stl_path

--- backend/extractor/merged/pullFromPrint.py
+++ backend/extractor/merged/pullFromPrint.py
@@ -8,8 +8,14 @@ import os
 
 base_url='http://143.239.73.224/api/v1/printer'
 
-gcode_path = '/Users/sb36/CS3300-Project/backend/extractor/merged/UMS5__3DBenchy.gcode' #change to your files path
+# absolute path (initial code)
+'''
+gcode_path = '/Users/sb36/CS3300-Project/backend/extractor/mergedsers/sb36/CS3300-Project/backend/extractor/merged/UMS5__3DBenchy.gcode' #change to your files path
 stl_path = '/Users/sb36/CS3300-Project/backend/extractor/merged/_3DBenchy.stl'          #change to your files path
+'''
+# relative path (universal)
+gcode_path = 'UMS5__3DBenchy.gcode'
+stl_path = '_3DBenchy.stl'
 
 endpoints = {
     "bed_temp": "/bed/temperature",
