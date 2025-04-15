e3e74e5891d65f6cdd50cef06c09c2466d758af5
SbZiggy123 <122312906@umail.com>
2025-04-15 13:35:42 +0100

    removed dot print and added comments to pullFromPrint paths

--- backend/extractor/merged/pullFromPrint.py
+++ backend/extractor/merged/pullFromPrint.py
@@ -8,12 +8,12 @@ import os
 
 base_url='http://143.239.73.224/api/v1/printer'
 
-# absolute path (initial code)
+# absolute path (initial code) -VSCode
 '''
-gcode_path = '/Users/sb36/CS3300-Project/backend/extractor/mergedsers/sb36/CS3300-Project/backend/extractor/merged/UMS5__3DBenchy.gcode' #change to your files path
+gcode_path = '/Users/sb36/CS3300-Project/backend/extractor/merged/UMS5__3DBenchy.gcode' #change to your files path
 stl_path = '/Users/sb36/CS3300-Project/backend/extractor/merged/_3DBenchy.stl'          #change to your files path
 '''
-# relative path (universal)
+# relative path (universal) -Pycharm
 gcode_path = 'UMS5__3DBenchy.gcode'
 stl_path = '_3DBenchy.stl'
 
