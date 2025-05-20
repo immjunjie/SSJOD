76519e93ca9cc8ba9615fc014482f4e913390b4d
makapaka122333 <oskarplonski1@yahoo.com>
2025-05-20 12:25:31 +0100

    Removed layer

--- backend/logger.py
+++ backend/logger.py
@@ -118,12 +118,12 @@ def run_logger_with_socket(socketio, hdf5_filename, base_url, camera_url, endpoi
         screenshots_grp.attrs['count'] = 0
 
         layers_grp = f.create_group('layers')
-        layer_grp = layers_grp.create_group(f'layer_{layer:04d}')
 
         executor = concurrent.futures.ThreadPoolExecutor(max_workers=len(endpoints))
 
         start_time = time.time()
 
+        first_scan = True
         try:
             while running:
                 if max_duration is not None and (time.time() - start_time) >= max_duration:
@@ -144,7 +144,16 @@ def run_logger_with_socket(socketio, hdf5_filename, base_url, camera_url, endpoi
                     position_xyz = np.array([0.0, 0.0, 0.0])
 
                 current_z = position_xyz[2]
-                if (current_z >= last_z + (layer_height - 0.05) and current_z <= last_z + (layer_height + 0.05)) or last_z == 0: #BRACKETS
+
+                if first_scan:
+                    layer = int(round(current_z / layer_height))
+                    layer_grp = layers_grp.create_group(f'layer_{layer:04d}')
+                    layer_grp.attrs['timestamp'] = timestamp
+                    last_z = current_z
+                    first_scan = False
+                    print(f"Starting at layer {layer} (Z={current_z:.3f} mm)")
+
+                elif (current_z >= last_z + (layer_height - 0.05) and current_z <= last_z + (layer_height + 0.05)) or last_z == 0: #BRACKETS
                     layer += 1
                     layer_grp = layers_grp.create_group(f'layer_{layer:04d}')
                     layer_grp.attrs['timestamp'] = timestamp
@@ -152,7 +161,7 @@ def run_logger_with_socket(socketio, hdf5_filename, base_url, camera_url, endpoi
                     print('Layer changed:', layer)
 
                     # Capture snapshot for this layer if snapshotter is initialized
-                    if snapshotter and layer > 0:  # Skip layer 0
+                    if snapshotter:  # Skip layer 0
                         print(f"Capturing snapshot for layer {layer}")
                         snapshotter.capture_layer_snapshot(layer)
 
